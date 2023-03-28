4c0844457549ea63f41ed88b43c661d27ed20b67
JoelCarr619 <JoelCarr619@gmail.com>
2023-03-28 10:27:49 -0700

    week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { log } = require("console");
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -12,41 +14,99 @@
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
+// Psuedo Code:
+// Takes in a string as a parameter.
+// I will define the mapping of the object.
+// call the function with each variable as an argument.
+// Iterate over each charate in the string.
+// return the encoded message as a string.
+
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// const secretCodeWord1 = "Lackadaisical";
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook";
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric";
+// // Expected output: "3cc3ntr1c"
 
-// b) Create the function that makes the test pass.
+// console.log(hiddenMessage(secretCodeWord1));
+// console.log(hiddenMessage(secretCodeWord2));
+// console.log(hiddenMessage(secretCodeWord3));
+
+// // b) Create the function that makes the test pass.
+
+// // can't seem to get this to run
+
+// const hiddenMessage = (message) => {
+//   message = message.replace(/a/g, "4");
+//   message = message.replace(/e/g, "3");
+//   message = message.replace(/I/g, "1");
+//   message = message.replace(/o/g, "0");
+//   return message;
+//   return message.replace(/[aeio]/g, (c) => ({ a: 4, e: 3, I: 1, o: 0 }[c]));
+// };
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
+// Pseudo Code
+// Create a function that takes in an array of words and a single letter.
+// returns an array of all the words containing that specific letter
+//  attempt to use the .filter method on the input array
+//  will use the .includes metyhod to check if it contains letter
+
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"];
 
-const letterA = "a"
+const letterA = "a";
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
+const letterE = "e";
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+// Solve for 'A'
+const solve1 = missingLetter(fruitArray, letterA);
+console.log(solve1);
+
+// Solve for 'E'
+const solve2 = missingLetter(fruitArray, letterE);
+console.log(solve2);
 // b) Create the function that makes the test pass.
+
+const missingLetter = (words, letter) => {
+  return words.filter((words) => words.includes(letter));
+};
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
+// Pseudo Code
+// Create a function that takes in an array of 5 numbers
+// sort the array
+// if either condition is true, return true, else return false.
+
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+const hand1 = [5, 5, 5, 3, 3];
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
+const hand2 = [5, 5, 3, 3, 4];
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+const hand3 = [5, 5, 5, 5, 4];
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
+const hand4 = [7, 2, 7, 2, 7];
 // Expected output: true
 
+expect(sitCom(hand1)).toBe(true);
+expect(sitCom(hand2)).toBe(false);
+expect(sitCom(hand3)).toBe(false);
+expect(sitCom(hand4)).toBe(true);
+
 // b) Create the function that makes the test pass.
+
+const sitCom = (hand) => {
+  const counts = {};
+  for (const num of hand) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+  const values = Object.values(counts);
+  return values.includes(2) && values.includes(3);
+};
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,12 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def find_words_containing_letter(beverages_array, letter)
+    beverages_array.select { |word| word.include?(letter)}
+end
+puts find_words_containing_letter(beverages_array, letter_o)
+puts find_words_containing_letter(beverages_array, letter_t)
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,10 +23,39 @@ letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def hash_to_array(hash)
+    array = []
+    has.each do |key, value|
+        array << value
+    end
+    array.sort
+end
+
+us_states = {
+    "Oregon" => "OR", 
+    "Alabama" => "AL", 
+    "New Jersey" => "NJ",
+     "Colorado" => "CO"
+}
+
+p hash_to_array(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+
+class Bike
+    def initialize(model, wheels=2, current_speed=o)
+        @model = model
+        @wheels = wheels@current_speed = current_speed
+
+    end
+    
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is currently going #{current_speed} mph."
+    end
+end
+
 
 
 
@@ -30,3 +65,21 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+    def initialize(speed)
+      @speed = speed
+    end
+  
+    def pedal_faster(speed_increase)
+      @speed += speed_increase
+      @speed = 0 if @speed < 0
+    end
+  
+    def brake(speed_decrease)
+      @speed -= speed_decrease
+      @speed = 0 if @speed < 0
+    end
+  end
+  
+  #
